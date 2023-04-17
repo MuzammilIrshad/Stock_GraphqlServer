05b5671ca2c6dfbfcd2c706dd437a3dc1e0ba354
abdulwaqar844 <abdulwaqar844@gmail.com>
2023-04-17 11:43:33 +0500

    added cors origin

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import { ApolloServer } from 'apollo-server';
 import typeDefs from './src/typeDefs.js';
-import  resolvers from './src/resolvers.js';
+import resolvers from './src/resolvers.js';
 
 const port = process.env.PORT || 3001;
 
-const server = new ApolloServer({ resolvers, typeDefs });
+const server = new ApolloServer({
+    resolvers, typeDefs, cors: {
+        origin: ['https://joulukaunistused.ee/', 'https://jade-moonbeam-0d3391.netlify.app/'],
+        credentials: true
+    }
+});
 server.listen({ port }, () => console.log(`Server runs at: http://localhost:${port}/graphql`));
