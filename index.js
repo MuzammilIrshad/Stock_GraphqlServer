69946d12ccae39ccd101fb1344a46c1e6cf43d9f
Abdul Waqar <63345475+abdulwaqar844@users.noreply.github.com>
2023-04-17 12:00:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-import { ApolloServer } from 'apollo-server';
-import typeDefs from './src/typeDefs.js';
-import resolvers from './src/resolvers.js';
+import { ApolloServer } from "apollo-server";
+import typeDefs from "./src/typeDefs.js";
+import resolvers from "./src/resolvers.js";
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4000;
 
 const server = new ApolloServer({
-    resolvers, typeDefs, cors: {
-        origin: ['https://joulukaunistused.ee/', 'https://jade-moonbeam-0d3391.netlify.app/'],
-        credentials: true
-    }
+  resolvers,
+  typeDefs,
+  cors: {
+    origin: 'https://joulukaunistused.ee/',
+    credentials: true
+  }
 });
-server.listen({ port }, () => console.log(`Server runs at: http://localhost:${port}/graphql`));
+server.listen({ port }, () =>
+  console.log(`Server runs at: http://localhost:${port}/graphql`)
+);
